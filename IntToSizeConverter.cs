f274b243566a048425e8569f104c682a7b2ac224
yiki.tsuneyama <at12k313@gmail.com>
2018-08-07 11:27:12 +0900

    VirtualizingWrapPanelをnugetから取得するように

---
+++ IntToSizeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SvgViewer
+{
+    public class IntToSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Size((double)value, (double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
